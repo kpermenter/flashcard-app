7f32db8450cb4535325f85110c5e46ae17219257
Kate Permenter <kpermenterhtx@gmail.com>
2019-12-29 14:09:49 -0600

    route handlers

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
+// middleware functions
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// PUG templates
 app.set('view engine', 'pug');
 
-//create route route on app object
-app.get('/', (req, res) => {
-  res.render('index');
+// require routes module
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards')
+
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
+
+// error handlers
+app.use((req, res, next) => {
+    const err = new Error('Not Found.');
+    err.status = 404;
+    next(err);
 });
 
-app.get('/cards', (req, res) => {
-  res.render('card', { prompt: "Who is buried in Grant's tomb?", hint:"Think about whose tomb it is." });
+app.use((req, res, next) => {
+    console.log('hello');
+    const err = new Error('Could not complete request.');
+    err.status = 500;
+    next(err);
 });
 
-app.listen(3000, function(){
-  console.log('port 3000 serving')
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
+
+// server
+app.listen(3000, function () {
+    console.log('port 3000 serving')
 });
