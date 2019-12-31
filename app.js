ea7f5383ab3a113595bf7d7821436317cf19feeb
Kate Permenter <kpermenterhtx@gmail.com>
2019-12-30 18:16:45 -0600

    first working version

--- app.js
+++ app.js
@@ -4,41 +4,30 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-// middleware functions
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
+app.use('/static', express.static('public'));
 
-// PUG templates
 app.set('view engine', 'pug');
 
-// require routes module
 const mainRoutes = require('./routes');
-const cardRoutes = require('./routes/cards')
+const cardRoutes = require('./routes/cards');
 
 app.use(mainRoutes);
 app.use('/cards', cardRoutes);
 
-// error handlers
 app.use((req, res, next) => {
-    const err = new Error('Not Found.');
-    err.status = 404;
-    next(err);
-});
-
-app.use((req, res, next) => {
-    console.log('hello');
-    const err = new Error('Could not complete request.');
-    err.status = 500;
-    next(err);
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 app.use((err, req, res, next) => {
-    res.locals.error = err;
-    res.status(err.status);
-    res.render('error');
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
 });
 
-// server
-app.listen(3000, function () {
-    console.log('port 3000 serving')
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!')
 });
--- routes/index.js
+++ routes/index.js
@@ -4,29 +4,29 @@ const router = express.Router();
 router.get('/', (req, res) => {
     const name = req.cookies.username;
     if (name) {
-        res.render('index', { name });
+      res.render('index', { name });
     } else {
-        res.redirect('/hello');
+      res.redirect('/hello');
     }
 });
 
 router.get('/hello', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.redirect('/');
-    } else {
-        res.render('hello');
-    }
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect('/');
+  } else {
+    res.render('hello');
+  }
 });
 
 router.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
+  res.cookie('username', req.body.username);
+  res.redirect('/');
 });
 
 router.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
+  res.clearCookie('username');
+  res.redirect('/hello');
 });
 
 module.exports = router;
